ba97a46e451af83592a35f3b6ee666f1eb13fa40
Vladislav Ulianeev <vladis.ulian@gmail.com>
2022-11-07 22:53:16 +0200

    \

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
 // Add imports above this line
+
+// Описан в документации
+// import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
 import { galleryItems } from './gallery-items';
+
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+const galleryIMGItems = createGalleryItems(galleryItems);
+galleryList.insertAdjacentHTML('beforeend', galleryIMGItems);
+
+function createGalleryItems(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <li>
+        <a class="gallery__item" href="${original}" >
+          <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+          />
+        </a>
+      </li>
+        `;
+    })
+    .join('');
+}
+const gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: '250',
+});
