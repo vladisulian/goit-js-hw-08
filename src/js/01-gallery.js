176d5bbea28324545157c44d0a2b6f008ce7a25a
Vladislav Ulianeev <vladis.ulian@gmail.com>
2022-11-08 00:13:44 +0200

    1 task is done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,8 +1,9 @@
 // Add imports above this line
-
 // Описан в документации
-// import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from 'simplelightbox';
 // Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 import { galleryItems } from './gallery-items';
 
 // Change code below this line
@@ -17,16 +18,14 @@ function createGalleryItems(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
       return `
-      <li>
-        <a class="gallery__item" href="${original}" >
-          <img
-            class="gallery__image"
-            src="${preview}"
-            alt="${description}"
-          />
+      <div class ="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" 
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"/>
         </a>
-      </li>
-        `;
+      </div>`;
     })
     .join('');
 }
