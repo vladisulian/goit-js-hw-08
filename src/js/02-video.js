03ed4852a99e11b98100f8a802b1f3be56e28746
Vladislav Ulianeev <vladis.ulian@gmail.com>
2022-11-10 17:56:10 +0200

    2 task is done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,13 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+player.on('timeupdate', throttle(onPlay, 1000));
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
- 
+function onPlay({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
