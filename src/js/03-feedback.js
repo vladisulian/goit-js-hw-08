c6c493b4f3fddeabae69e06bac01c0f2e56c82cf
Vladislav Ulianeev <vladis.ulian@gmail.com>
2022-11-12 18:56:21 +0200

    task is done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,18 +1,41 @@
 const formElements = {
   form: document.querySelector('.feedback-form'),
+  mail: document.querySelector('input'),
   textarea: document.querySelector('.feedback-form textarea'),
 };
 
 formElements.form.addEventListener('submit', onFormSubmit);
+formElements.mail.addEventListener('input', onMailInput);
 formElements.textarea.addEventListener('input', onTextareaInput);
+
+populateTextarea();
 
 function onFormSubmit(e) {
   e.preventDefault();
-  console.log('Form submit');
   e.currentTarget.reset();
+  localStorage.removeItem('feedback-message');
 }
 
 function onTextareaInput(e) {
   const message = e.currentTarget.value;
-  localStorage.setItem('feedback-meesage', message);
+  localStorage.setItem('feedback-message', message);
+}
+
+function onMailInput(e) {
+  const mail = e.currentTarget.value;
+  localStorage.setItem('feedback-mail', mail);
+}
+
+function populateTextarea() {
+  const savedMessage = localStorage.getItem('feedback-message');
+  const savedMail = localStorage.getItem('feedback-mail');
+
+  if (savedMessage && savedMail) {
+    formElements.textarea.value = savedMessage;
+    formElements.mail.value = savedMail;
+  } else if (savedMessage) {
+    formElements.textarea.value = savedMessage;
+  } else if (savedMail) {
+    formElements.mail.value = savedMail;
+  }
 }
