a3196bae7fc16fea726f41fa4dafdc706b0e8e63
Vladislav Ulianeev <vladis.ulian@gmail.com>
2022-11-12 14:49:36 +0200

    Upd. Now text is have saved on local storage

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,18 @@
+const formElements = {
+  form: document.querySelector('.feedback-form'),
+  textarea: document.querySelector('.feedback-form textarea'),
+};
 
+formElements.form.addEventListener('submit', onFormSubmit);
+formElements.textarea.addEventListener('input', onTextareaInput);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  console.log('Form submit');
+  e.currentTarget.reset();
+}
+
+function onTextareaInput(e) {
+  const message = e.currentTarget.value;
+  localStorage.setItem('feedback-meesage', message);
+}
