511b6f84e56db686e7035f0ebbe641369a0dac18
vladisulian <106911452+vladisulian@users.noreply.github.com>
2022-11-12 12:50:01 +0000

    Deploying to gh-pages from @ vladisulian/goit-js-hw-08@a3196bae7fc16fea726f41fa4dafdc706b0e8e63 🚀

---
+++ 03-feedback.0225ec46.js
@@ -0,0 +1,2 @@
+const e={form:document.querySelector(".feedback-form"),textarea:document.querySelector(".feedback-form textarea")};e.form.addEventListener("submit",(function(e){e.preventDefault(),console.log("Form submit"),e.currentTarget.reset()})),e.textarea.addEventListener("input",(function(e){const t=e.currentTarget.value;localStorage.setItem("feedback-meesage",t)}));
+//# sourceMappingURL=03-feedback.0225ec46.js.map
