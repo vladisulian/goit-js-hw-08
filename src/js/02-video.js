977a681e2bf07f5e65a077c4d0f2fc5095a035ac
Vladislav Ulianeev <vladis.ulian@gmail.com>
2022-11-15 21:18:55 +0200

    1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,17 +4,20 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('timeupdate', throttle(onPlay, 1000));
+player.on(
+  'timeupdate',
+  throttle(element => {
+    localStorage.setItem(
+      'videoplayer-current-time',
+      JSON.stringify(element.seconds)
+    );
+  }, 1000)
+);
 
-function onPlay(data) {
-  localStorage.setItem(
-    'videoplayer-current-time',
-    JSON.stringify(data.seconds)
-  );
+if (localStorage.getItem('videoplayer-current-time') !== null) {
+  player
+    .setCurrentTime(Number(localStorage.getItem('videoplayer-current-time')))
+    .catch(function (error) {
+      console.error(error);
+    });
 }
-
-// if (localStorage.videoplayer-current-time) {
-//   player.setCurrentTime(Number(localStorage.getItem('videoplayer-current-time')));
-// }
-
-player.setCurrentTime(Number(localStorage.getItem('videoplayer-current-time')));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,6 +5,8 @@ const formElements = {
 };
 
 formElements.form.addEventListener('submit', onFormSubmit);
+formElements.mail.addEventListener('input', onMailInput);
+formElements.textarea.addEventListener('input', onTextareaInput);
 
 populateTextarea();
 
@@ -12,24 +14,29 @@ function onFormSubmit(e) {
   e.preventDefault();
   e.currentTarget.reset();
   localStorage.removeItem('feedback-message');
+  localStorage.removeItem('feedback-mail')
 }
 
-formElements.form.addEventListener('input', onFormInput);
-function onFormInput(e) {
+function onTextareaInput(e) {
   const message = e.currentTarget.value;
   localStorage.setItem('feedback-message', message);
 }
 
-// function populateTextarea() {
-//   const savedMessage = localStorage.getItem('feedback-message');
-//   const savedMail = localStorage.getItem('feedback-mail');
+function onMailInput(e) {
+  const mail = e.currentTarget.value;
+  localStorage.setItem('feedback-mail', mail);
+}
 
-//   if (savedMessage && savedMail) {
-//     formElements.textarea.value = savedMessage;
-//     formElements.mail.value = savedMail;
-//   } else if (savedMessage) {
-//     formElements.textarea.value = savedMessage;
-//   } else if (savedMail) {
-//     formElements.mail.value = savedMail;
-//   }
-// }
+function populateTextarea() {
+  const savedMessage = localStorage.getItem('feedback-message');
+  const savedMail = localStorage.getItem('feedback-mail');
+
+  if (savedMessage && savedMail) {
+    formElements.textarea.value = savedMessage;
+    formElements.mail.value = savedMail;
+  } else if (savedMessage) {
+    formElements.textarea.value = savedMessage;
+  } else if (savedMail) {
+    formElements.mail.value = savedMail;
+  }
+}
